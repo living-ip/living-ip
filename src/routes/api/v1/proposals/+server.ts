4d789800aea91361b3674b39707b6f8051d5e562
Mike MacCana <mike.maccana@gmail.com>
2023-06-15 18:46:20 +0100

    API cleanups

--- src/routes/api/v1/proposals/+server.ts
+++ src/routes/api/v1/proposals/+server.ts
@@ -1,6 +1,6 @@
 // https://kit.svelte.dev/docs/routing#server
 import { log, stringify } from '$lib/functions';
-import { getPullRequests } from '$lib/get-pull-requests';
+import { getGithubUsername,  getPullRequests } from '$lib/get-pull-requests';
 import { error as makeHTTPError, json as makeJSONResponse, type RequestHandler } from '@sveltejs/kit';
 import type {  Db }  from 'mongodb';
 
@@ -9,6 +9,11 @@ import { getTotalValue } from '$lib/utils';
 
 // http://localhost:5173/api/v1/proposals?walletAddress=5FHwkrdxntdK24hgQU8qgBjn35Y1zwhz1GZwCkP2UJnM&githubAccessToken=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  }
 
+
+// GET /api/v1/proposals
+// Params
+// - walletAddress
+// - githubAccessToken
 export const GET = (async (request) => {
   log(`Handling GET request to /api/v1/proposals`);
 
@@ -29,9 +34,18 @@ export const GET = (async (request) => {
   const database = databaseOrNull as Db;
 
 
+
   const usersCollection = database.collection('users');
   const user = await usersCollection.findOne({ walletAddress });
 
+  
+  try {
+    await getGithubUsername(user.githubAccessToken);
+  } catch (error) {
+    if ( error.message.includes('Bad credentials') ) { 
+      throw makeHTTPError(400, `Invalid githubAccessToken: ${user.githubAccessToken}`);
+    }
+  }
   const pullRequests = await getPullRequests(githubAccessToken, REPO_OWNER, REPO);
 
   const userPullRequests = pullRequests.filter(pullRequest => pullRequest.user === user.githubUsername);
@@ -40,7 +54,7 @@ export const GET = (async (request) => {
   const total = getTotalValue(mergedPullRequests, user.githubUsername);
 
 
-  return json({
+  return makeJSONResponse({
     userPullRequests,
     mergedPullRequests,
     unmergedPullRequests,
