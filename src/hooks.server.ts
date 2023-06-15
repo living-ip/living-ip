4e1d4d230da6d3ba995b96fc7322727fc16cac82
Mike MacCana <mike.maccana@gmail.com>
2023-06-15 18:46:59 +0100

    Temp workaround Mongo not connected issue

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,20 @@
 // https://kit.svelte.dev/docs/hooks
+// "Code in these modules will run when the application starts up, making them useful for initializing database clients and so on."
+
+// TODO: this doesn't actually seem to be true
+// logs occur multiple times
 
 import { log, stringify } from '$lib/functions';
 import { getMongoClient } from '$lib/server-utils';
 import type { Handle } from "@sveltejs/kit";
 
-log(`🧺Connecting to MongoDB...`);
-const mongoClient = await getMongoClient();
-await mongoClient.connect();
-const database = mongoClient.db('decentralizedIP');
-log(`🧺 connected to MongoDB!`);
-
 export const handle = (async ({ event, resolve }) => {
-  
+  const mongoClient = await getMongoClient();
   try { 
+    log(`🧺Connecting to MongoDB...`);
+    await mongoClient.connect();
+    const database = mongoClient.db('decentralizedIP');
+    log(`🧺 connected to MongoDB!`);
     
     event.locals = { database };
 
