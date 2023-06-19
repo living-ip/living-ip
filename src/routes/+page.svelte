04a990f47b40cf1179a7b6027619741a39970d68
Mike MacCana <mike.maccana@gmail.com>
2023-06-19 14:22:40 +0100

    Add Skeleton UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -132,7 +132,24 @@
     <h1>Login to github</h1>
     <a class="button github-login" href="/oauth">Login to github </a>
   {:else if showLoadingScreen}
-    <p>Loading Project</p>
+    <!-- A skeleton loading UI -->
+    <div class="title-and-refresh">
+      <title class="app-name-skeleton skeleton" />
+    </div>
+
+    <div class="tabs">
+      <div class="tab-skeleton skeleton" />
+      <div class="tab-skeleton skeleton" />
+    </div>
+
+    <div class="reward-summary-and-proposals">
+      <div class="rewards-skeleton skeleton" />
+      <div class="rewards-proposals-skeleton skeleton">
+        <div class="rewards-proposal-skeleton skeleton" />
+        <div class="rewards-proposal-skeleton skeleton" />
+        <div class="rewards-proposal-skeleton skeleton" />
+      </div>
+    </div>
   {:else}
     <div class="title-and-refresh">
       <title class="app-name">{repo}</title><button class="refresh" on:click={refreshRepo} />
@@ -229,6 +246,25 @@
     line-height: 24px;
     font-weight: 400;
   }
+  @keyframes skeleton-background-shine {
+    to {
+      background-position: 100% 0, /* move highlight to right */ 0 0;
+    }
+  }
+
+  .skeleton {
+    background: linear-gradient(
+        to right,
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 0.5) 50%,
+        rgba(255, 255, 255, 0) 80%
+      ),
+      lightgray;
+    background-repeat: repeat-y;
+    background-size: 50px 200px;
+    background-position: 0 0;
+    animation: skeleton-background-shine 1s infinite;
+  }
 
   h1 {
     font-size: 32px;
@@ -304,6 +340,13 @@
     font-weight: 600;
   }
 
+  .app-name-skeleton {
+    width: 162px;
+    height: 28px;
+    background-color: #e0e0e0;
+    border-radius: 3px;
+  }
+
   .reward-summary-and-proposals {
     grid-auto-flow: column;
     grid-template-columns: 1fr 1fr;
@@ -311,6 +354,20 @@
     justify-items: left;
     align-items: start;
     width: 100%;
+  }
+
+  .rewards-skeleton {
+    width: 100%;
+    height: 284px;
+    background-color: #e0e0e0;
+    border-radius: 3px;
+  }
+
+  .rewards-proposals-skeleton {
+    width: 100%;
+    height: 500px;
+    background-color: #e0e0e0;
+    border-radius: 3px;
   }
 
   section {
@@ -365,6 +422,10 @@
     border-bottom: 0.5px solid #222;
   }
 
+  .tabs:has(.skeleton) {
+    border-bottom: none;
+  }
+
   .tab {
     width: 96px;
     font-size: 16px;
@@ -381,5 +442,12 @@
 
   .tab.active {
     font-weight: 600;
+  }
+
+  .tab-skeleton {
+    width: 96px;
+    height: 38px;
+    background-color: #e0e0e0;
+    border-radius: 3px;
   }
 </style>
