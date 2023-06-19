fa86c7e0ec4dd5cf0f97d1e5f812c54eaeed813f
Mike MacCana <mike.maccana@gmail.com>
2023-06-19 14:01:40 +0100

    Remove unused code from mongo-admin

--- mongo-admin.ts
+++ mongo-admin.ts
@@ -4,29 +4,27 @@ import { getMongoClient } from './src/lib/server-utils';
 const ASCENDING = 1;
 
 const mongoClient = await getMongoClient();
-try {
-  await mongoClient.connect();
-  const database = mongoClient.db('decentralizedIP');
+const database = mongoClient.db('decentralizedIP');
 
-  // Example of adding a user:
-  // const usersCollection = await database.createCollection('users');
-  // await usersCollection.insertMany([
-  //   {
-  //     walletAddress: '5FHwkrdxntdK24hgQU8qgBjn35Y1zwhz1GZwCkP2UJnM',
-  //     walletName: 'mikemaccana.sol',
-  //     githubUsername: 'mikemaccana',
-  //   },
-  // ]);
-  //
-  // const documentCount = await usersCollection.countDocuments();
-  // log(`${documentCount} users`);
-} catch (thrownObject) {
-  const error = thrownObject as Error;
-  if (error.message.includes('certificate validation failed')) {
-    throw new Error(`Error connecting to MongoDB: the .pem file mentioned in the .env file is missing or invalid.`);
-  }
-  log(`Error connecting to MongoDB`, error.message);
-} finally {
-  // Ensures that the client will close when you finish/error
-  await mongoClient.close();
-}
+// Example of adding a user:
+const usersCollection = await database.collection('users');
+
+// const mike = await usersCollection.findOneAndUpdate(
+//   { walletAddress: '5FHwkrdxntdK24hgQU8qgBjn35Y1zwhz1GZwCkP2UJnM' },
+//   {
+//     $set: {
+//       walletName: 'mikemaccana.sol',
+//       githubUsername: 'mikemaccana',
+//       profilePicture: 'https://cdn.glow.app/g/er/ae01b288-3bc6-4248-ac49-a6b6c6132fb6',
+//     },
+//   },
+//   { upsert: true },
+// );
+
+// log(`updated mike`, mike);
+
+// get all documents from the users collection\
+const users = await usersCollection.find({}).toArray();
+log(users);
+
+process.exit(0);
