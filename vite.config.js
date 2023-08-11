38370126b01132663799facaddf2b804e4e46311
Mike MacCana <mike.maccana@gmail.com>
2023-08-11 20:33:52 +0100

    Fix 'has been externalized for browser compatibility' warnings from vite

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,19 @@ const config = {
     alias: {
       $utils: path.resolve('src/utils/'),
       stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      // EMPTY polyfills
+      // some of our dependencies have dependencies for
+      // stupid things - other blockchains, nodejs, etc.
+      // these never get run in the browser so we define them to be empty.
+      // This fixes a bunch of
+      // 'has been externalized for browser compatibility'
+      // warnings from vite
+      crypto: 'rollup-plugin-node-polyfills/polyfills/empty',
+      https: 'rollup-plugin-node-polyfills/polyfills/empty',
+      zlib: 'rollup-plugin-node-polyfills/polyfills/empty',
+      url: 'rollup-plugin-node-polyfills/polyfills/empty',
+      http: 'rollup-plugin-node-polyfills/polyfills/empty',
+      path: 'rollup-plugin-node-polyfills/polyfills/empty',
     },
   },
   define: {
